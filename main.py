a966a4731d0f96dd34d17a9e2cb28c7e7793c995
warapong-pj <warapong-pj@gmail.com>
2025-11-09 06:58:30 +0700

    create sample fastapi

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from devsecops!")
+from fastapi import FastAPI
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
